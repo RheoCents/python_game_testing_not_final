635bb7f7996d5b463372f3f181828554e93e24a3
unknown <centinaraphael0157@gmail.com>
2024-02-02 18:56:44 +0800

    background upload

--- python_platformer.py
+++ python_platformer.py
@@ -4,10 +4,17 @@ from pygame.locals import *
 pygame.init()
 
 game_screen = pygame.display.set_mode((1000,650))
-pygame.display.set_caption("rheo's world")
+pygame.display.set_caption("Rheo's world")
+
+#images 
+bg_image = pygame.image.load('Untitled design (2).png')
+
 game_on = True
 while game_on:
+    game_screen.blit(bg_image, (0,0,))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game_on = False
+    
+    pygame.display.update()
 pygame.quit()
